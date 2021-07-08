0e7692f8ef762ecb9a835f983a7f476bd52652b6
Brendan Linn <brendan.linn@gmail.com>
2021-07-08 08:34:47 -0700

    mvce for https://github.com/jOOQ/jOOQ/issues/12118
    to verify: `mvn clean && mvn generate-sources`
    
    the generated src/main/java/org/jooq/mcve/java/Keys.java will contain
    the following declaration, which is a compilation error in javac 9+
    due to the underscore:
    
    ```
    public static final UniqueKey<AliasesRecord> _ =
    Internal.createUniqueKey(Aliases.ALIASES, DSL.name(""), new
    TableField[] { Aliases.ALIASES.ID, Aliases.ALIASES.ALIAS }, true);
    ```

--- src/main/resources/db/migration/V1__initialise_database.sql
+++ src/main/resources/db/migration/V1__initialise_database.sql
@@ -1,10 +1,5 @@
-DROP SCHEMA IF EXISTS mcve CASCADE;
-
-CREATE SCHEMA mcve;
-
-CREATE TABLE mcve.test (
-  id    INT NOT NULL AUTO_INCREMENT,
-  value INT,
-  
-  CONSTRAINT pk_test PRIMARY KEY (id) 
+CREATE TABLE aliases (
+  id text,
+  alias text,
+  PRIMARY KEY (id, alias)
 );
